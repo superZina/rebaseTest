78b80cf6abb5608c96eb7ce555f2db775935e4a6
이진하 <dkwlsfk2323@gmail.com>
2022-03-10 11:44:59 +0900

    delete key chain and push

--- test/test/main.swift
+++ test/test/main.swift
@@ -9,10 +9,6 @@ import Foundation
 
 print("Hello, World!")
 print("Hello, World!")
-print("Hello, World!")
 
 print("Hello, World!")
 print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-
